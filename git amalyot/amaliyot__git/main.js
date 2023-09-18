b49b2fd8b246037a33e7e9aaae120412cc5d3aeb
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-18 23:08:11 +0500

    js

---
+++ git amalyot/amaliyot__git/main.js
@@ -0,0 +1,7 @@
+const number = document.querySelector("number");
+const step = document.querySelector(".step");
+const time = document.querySelector(".time");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+
+
