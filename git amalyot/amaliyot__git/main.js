f782e7939e353e866fe81a433ae081e33c3a6262
Muhammad <muhammadjonrasuljonov2002@gmail.com>
2023-09-18 23:45:10 +0500

    uy ishi

--- git amalyot/amaliyot__git/main.js
+++ git amalyot/amaliyot__git/main.js
@@ -1,7 +1,27 @@
-const number = document.querySelector("number");
+const number = document.querySelector(".number");
 const step = document.querySelector(".step");
 const time = document.querySelector(".time");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 
+btn1.addEventListener("click", () => {
+	if (step.value !== "" && time.value == "") {
+		console.log(step.value);
+		number.innerText = +step.value + +number.innerText;
+	} else {
+		setInterval(() => {
+			number.innerText = +step.value + +number.innerText;
+		}, time.value * 1000);
+	}
+});
 
+btn2.addEventListener("click", () => {
+	if (step.value !== "" && time.value == "") {
+		console.log(step.value);
+		number.innerText = +number.innerText - +step.value;
+	} else {
+		setInterval(() => {
+			number.innerText = +number.innerText - +step.value ;
+		}, time.value * 1000);
+	}
+});
