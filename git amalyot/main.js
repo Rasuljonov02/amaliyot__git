1b0f43553028a2dd74f77267e6edd6e6f9a48c47
JahongirOdilov <odilovjahongir2001@gmail.com>
2023-09-19 11:50:45 +0500

    codes

--- git amalyot/main.js
+++ git amalyot/main.js
@@ -1,8 +1,14 @@
+/*  DOM VARIABLES */
+
+
 const number = document.querySelector(".number");
 const step = document.querySelector(".step");
 const time = document.querySelector(".time");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
+
+
+/*  HANDLER FUNCTION */
 
 btn1.addEventListener("click", () => {
 	if (step.value !== "" && time.value == "") {
